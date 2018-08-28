ce565f9db1d297a75810eadceaafc26a619904d7
Henry Szeto <henryszeto@gmail.com>
2018-08-27 23:32:43 -0700

    update production api endpoint

--- src/shared/apiEndPoints.js
+++ src/shared/apiEndPoints.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = {
-    domain: 'http://localhost:3333',
+    domain: 'https://todo-rails5-api.herokuapp.com',
   };
 } else {
   module.exports = {
